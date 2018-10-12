818b9345a442c606aad9c833541c2bf97e85eab1
Marcin Juraszek <marcinj@microsoft.com>
2018-10-12 08:46:38 -0700

    Add comment for future readers

--- src/WopiValidator.Core/Mutators/ProofKeyMutator.cs
+++ src/WopiValidator.Core/Mutators/ProofKeyMutator.cs
@@ -56,6 +56,7 @@ namespace Microsoft.Office.WopiValidator.Core.Mutators
 			Dictionary<string, string> originalProofKeyHeaders,
 			Func<long, Dictionary<string, string>> proofKeyGeneration)
 		{
+			// if we're mutating the wopi timestamp we have to regenerate the entire proofKey data before doing other mutations.
 			Dictionary<string, string> proofKeyHeaders
 				= WopiTimestamp != null ? proofKeyGeneration(WopiTimestamp.Value) : new Dictionary<string, string>(originalProofKeyHeaders);
 			
