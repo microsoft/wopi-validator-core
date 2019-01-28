d38b2b9587b09285f9519a55335cb561e154c438
Tyler Butler <tylerbu@microsoft.com>
2019-01-28 13:18:34 -0800

    whitespace

--- test/WopiValidator.Core.Tests/Factories/TestCaseFactoryUnitTests.cs
+++ test/WopiValidator.Core.Tests/Factories/TestCaseFactoryUnitTests.cs
@@ -15,8 +15,8 @@ namespace Microsoft.Office.WopiValidator.UnitTests.Factories
 		{
 			string inputValue = "   Line 1.\r\nLine 2.    ";
 			string expectedValue = "Line 1. Line 2.";
-			string actualValue = TestCaseFactory.CondenseMultiLineString(inputValue);
 
+			string actualValue = TestCaseFactory.CondenseMultiLineString(inputValue);
 			Assert.AreEqual(expectedValue, actualValue);
 		}
 
@@ -25,8 +25,8 @@ namespace Microsoft.Office.WopiValidator.UnitTests.Factories
 		{
 			string input = "   Line 1.\r\n\r\n     Line 2.   ";
 			string expectedValue = "Line 1." + Environment.NewLine + "Line 2.";
-			string actualValue = TestCaseFactory.CondenseMultiLineString(input);
 
+			string actualValue = TestCaseFactory.CondenseMultiLineString(input);
 			Assert.AreEqual(expectedValue, actualValue);
 		}
 
@@ -35,8 +35,8 @@ namespace Microsoft.Office.WopiValidator.UnitTests.Factories
 		{
 			string input = "Line 1.   \n    \n    \n     Line 2.    \n\n\nLine 3.        ";
 			string expectedValue = "Line 1." + Environment.NewLine + "Line 2." + Environment.NewLine + "Line 3.";
-			string actualValue = TestCaseFactory.CondenseMultiLineString(input);
 
+			string actualValue = TestCaseFactory.CondenseMultiLineString(input);
 			Assert.AreEqual(expectedValue, actualValue);
 		}
 
@@ -45,8 +45,8 @@ namespace Microsoft.Office.WopiValidator.UnitTests.Factories
 		{
 			string input = "Line 1.\n  \t   Line 2.   \n\nLine 3.   \n\n\n\n\n   \t  Line 4.";
 			string expectedValue = "Line 1. Line 2." + Environment.NewLine + "Line 3." + Environment.NewLine + "Line 4.";
-			string actualValue = TestCaseFactory.CondenseMultiLineString(input);
 
+			string actualValue = TestCaseFactory.CondenseMultiLineString(input);
 			Assert.AreEqual(expectedValue, actualValue);
 		}
 	}
