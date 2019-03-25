6937a31c7700fa5cf474d70e9ea6c344db0a9b07
Tyler Butler <tylerbu@microsoft.com>
2019-03-25 12:05:48 -0700

    Stop parsing strings as dates
    Fixes #74 (for real this time!)

---
+++ src/WopiValidator.Core/JsonHelper.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace Microsoft.Office.WopiValidator
+{
+	internal static class JsonHelper
+	{
+		internal static JObject ParseJObject(this string jsonString)
+		{
+			JsonReader reader = new JsonTextReader(new StringReader(jsonString))
+			{
+				DateParseHandling = DateParseHandling.None
+			};
+			JObject jObject = JObject.Load(reader);
+			return jObject;
+		}
+	}
+}
